46caa83ec45e70fc1b08786b13cf1bfead945a4e
Megan Dramski <36252981+MeganDramski@users.noreply.github.com>
2018-02-12 20:05:51 -0500

    Update Simple_Linear_Regression.py

--- Simple_Linear_Regression.py
+++ Simple_Linear_Regression.py
@@ -19,6 +19,7 @@ regressor.fit(X_train, y_train)
 
 #predict the test set results
 y_pred= regressor.predict(X_test)
+
 '''
 #Visualizing trainig results 
 plt.scatter(X_train,y_train , color= 'red')
@@ -27,6 +28,7 @@ plt.title('Salary vs Experiance(Traing Set)')
 plt.xlabel('Years of Experiance')
 plt.ylabel('Salary')
 plt.show()'''
+
 #VIsualizing test results 
 plt.scatter(X_test,y_test , color= 'red')
 plt.plot(X_train, regressor.predict(X_train), color='green')
