7c22af96b88cf90fa54deb8a108a801b63d7f828
Megan Dramski <36252981+MeganDramski@users.noreply.github.com>
2018-02-12 20:04:46 -0500

    Update Mnist_Data_CNN_Linear.py

--- Mnist_Data_CNN_Linear.py
+++ Mnist_Data_CNN_Linear.py
@@ -1,11 +1,9 @@
 
 import tensorflow as tf 
 from tensorflow.examples.tutorials.mnist import input_data
-
-
 mnist= input_data.read_data_sets("MNIST_data/", one_hot=True)
-
 import matplotlib.pyplot as plt
+
 # placeholders 
 x= tf.placeholder(tf.float32, shape= [None, 784])
 
@@ -18,12 +16,12 @@ y= tf.matmul(x,W) + b
 
 #loss function 
 y_true =tf.placeholder(tf.float32, [None,10])
-
 cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels= y_true, logits=y))
 
 #optimizer 
 optimizer = tf.train.GradientDescentOptimizer( learning_rate= 0.5)
 train=optimizer.minimize(cross_entropy)
+
 #create session
 init= tf.global_variables_initializer()
 with tf.Session() as sess:
@@ -35,7 +33,7 @@ with tf.Session() as sess:
         
         #elvaluate the model 
         correct_prediction= tf.equal( tf.argmax(y, 1), tf.argmax(y_true,1))
+        
         # true, false , true (1,0,1)
         acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-        
         print(sess.run(acc, feed_dict={x:mnist.test.images,y_true:mnist.test.labels}))
